c649850a6db92592a3b577bd3ec28a064c85281d
semnovik <semnovik@ozon.ru>
2022-06-09 23:27:09 +0200

    Got main idea

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,44 @@
-package hw02unpackstring
+package main
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var newStr string
+	strLen := utf8.RuneCountInString(str)
+
+	for i := 0; i <= strLen; i++ {
+		fmt.Println(str[i])
+	}
+
+	return newStr, nil
+}
+
+func main() {
+	var str = "a4bc2d5e"
+	var newStr = ""
+	var cache = ""
+	strLen := utf8.RuneCountInString(str)
+
+	for i := 0; i < strLen; i++ {
+
+		if unicode.IsDigit(rune(str[i])) == true {
+			num, _ := strconv.Atoi(string(str[i]))
+			fmt.Println("Тут цифра", num)
+			newStr += strings.Repeat(cache, num-1)
+		} else {
+			cache = string(str[i])
+			newStr += cache
+			fmt.Println(newStr)
+		}
+	}
+
 }
