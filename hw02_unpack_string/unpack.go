f6bf2a7a9d52cc3dee3dd4ac65a93b9627d4936c
semnovik <semnovik@ozon.ru>
2022-06-14 14:44:24 +0200

    lint

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -18,25 +18,20 @@ func Unpack(str string) (string, error) {
 		symbol := got[i]
 
 		switch {
-
 		// Проверяем, что первый символ не цифра
 		case i == 0 && unicode.IsDigit(symbol):
 			return "", ErrInvalidString
-
-		//Проверяем, что нет двухзначных цифр
+		// Проверяем, что нет двухзначных цифр
 		case unicode.IsDigit(symbol) && unicode.IsDigit(rune(got[i-1])):
 			return "", ErrInvalidString
-
-		//Если в строке содержится 0
+		// Если в строке содержится 0
 		case symbol == 48:
 			newStr = strings.Replace(newStr, cachedSymbol, "", 1)
-
-		//Если цифра после буквы
+		// Если цифра после буквы
 		case unicode.IsDigit(symbol) && symbol != 48:
 			num, _ := strconv.Atoi(string(got[i]))
 			newStr += strings.Repeat(cachedSymbol, num-1)
-
-		//Если буква
+		// Если буква
 		case !unicode.IsDigit(symbol):
 			cachedSymbol = string(got[i])
 			newStr += cachedSymbol
